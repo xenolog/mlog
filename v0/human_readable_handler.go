279b01414f8c0e09b374015428d04b3c7783c55e
Sergey Vasilenko <svasilenko@mirantis.com>
2023-11-21 18:43:04 +0300

    move consts to dedicated file

--- v0/human_readable_handler.go
+++ v0/human_readable_handler.go
@@ -13,20 +13,6 @@ import (
 	"sync"
 )
 
-const (
-	TimeInputFormatRFC3339  = "2006-01-02T15:04:05.99999Z07"
-	TimeOutputFormatRFC3339 = "2006-01-02T15:04:05.00000Z07"
-	LogLineBuffSize         = 1024
-	AttrsJSONprefix         = "ATTRS="
-)
-
-var level2Letter = map[slog.Level]string{ //nolint:gochecknoglobals
-	slog.LevelDebug: " D ",
-	slog.LevelInfo:  " I ",
-	slog.LevelWarn:  " W ",
-	slog.LevelError: " E ",
-}
-
 type jsonTree map[string]any
 
 type group struct {
